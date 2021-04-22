b64aa5936e26198338792460456999171a6752c2
rrobles-22 <70533022+rrobles-22@users.noreply.github.com>
2021-04-21 20:32:42 -0500

    Delete act311.hmwrk directory

--- act311.hmwrk/main.js
+++
@@ -1,5 +0,0 @@
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
