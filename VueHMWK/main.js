023834d31243012a4883209d4432aa47ab1aba94
rrobles-22 <70533022+rrobles-22@users.noreply.github.com>
2021-04-21 20:33:01 -0500

    Add files via upload

---
+++ VueHMWK/main.js
@@ -0,0 +1,5 @@
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
